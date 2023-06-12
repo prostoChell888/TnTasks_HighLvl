fd1c54090519e74580c549e09983e17387cd02ac
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-12 17:51:38 +0300

    add app

---
+++ src/main/java/ru/bahmutov/NoteApp.java
@@ -0,0 +1,13 @@
+package ru.bahmutov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApp.class, args);
+    }
+
+}
