a8f27170726c311d9c952e071f0d54fb7eabd3eb
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-12 21:19:16 +0300

    add repository

---
+++ src/main/java/ru/bahmutov/repositories/NoteRepository.java
@@ -0,0 +1,15 @@
+package ru.bahmutov.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.bahmutov.entity.Note;
+
+import java.util.Optional;
+
+@Repository
+public interface NoteRepository
+        extends JpaRepository<Note, Long> {
+
+    @Override
+    Optional<Note> findById(Long aLong);
+}
