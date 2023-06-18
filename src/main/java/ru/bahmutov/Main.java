84f0029b208205ec1f48bc7c1117086ef8e96193
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 15:53:00 +0300

    add example

--- src/main/java/ru/bahmutov/Main.java
+++ src/main/java/ru/bahmutov/Main.java
@@ -1,7 +1,16 @@
 package ru.bahmutov;
 
+import ru.bahmutov.configuration.ConfigurationDB;
+import ru.bahmutov.repository.UserRepository;
+import ru.bahmutov.repository.jdbc.UserJDBCRepository;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        var prop = new ConfigurationDB("application.properties");
+        UserRepository repository = new UserJDBCRepository(prop);
+        var res = repository.getAllUsers();
+        res.forEach(System.out::println);
     }
 }
