bbae069eaa8f2a33358e10cb04de87dd3a6c0593
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 09:18:46 +0300

    add dto

---
+++ src/main/java/ru/bahmutov/dao/BankDTO.java
@@ -0,0 +1,14 @@
+package ru.bahmutov.dao;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BankDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/ru/bahmutov/dao/UserDTO.java
@@ -0,0 +1,13 @@
+package ru.bahmutov.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String fullName;
+}
