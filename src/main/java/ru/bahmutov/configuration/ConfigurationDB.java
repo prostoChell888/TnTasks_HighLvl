1d5eb628254c955054c4c59bfb369a86996fb050
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 15:45:04 +0300

    add configuration class

---
+++ src/main/java/ru/bahmutov/configuration/ConfigurationDB.java
@@ -0,0 +1,31 @@
+package ru.bahmutov.configuration;
+
+import lombok.Getter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Getter
+public class ConfigurationDB {
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public ConfigurationDB(String configFile) {
+        Properties properties = loadProperties(configFile);
+        this.url = properties.getProperty("datasource.url");
+        this.username = properties.getProperty("datasource.username");
+        this.password = properties.getProperty("datasource.password");
+    }
+
+    private Properties loadProperties(String configFile) {
+        Properties properties = new Properties();
+            try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(configFile)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
