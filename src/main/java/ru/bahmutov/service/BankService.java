92d44e0169ba6bca1c048d74867cec686b7474e2
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 15:51:18 +0300

    change service interface

--- src/main/java/ru/bahmutov/service/BankService.java
+++ src/main/java/ru/bahmutov/service/BankService.java
@@ -2,9 +2,10 @@ package ru.bahmutov.service;
 
 import ru.bahmutov.dao.BankDTO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface BankService {
 
-    List<BankDTO> updateBankNames(String newName);
+    List<BankDTO> updateBankNames(String newName) throws SQLException;
 }
--- src/main/java/ru/bahmutov/service/UserService.java
+++ src/main/java/ru/bahmutov/service/UserService.java
@@ -2,10 +2,11 @@ package ru.bahmutov.service;
 
 import ru.bahmutov.dao.UserDTO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserService {
-    public interface UserRepository {
-        List<UserDTO> getAllUsers();
-    }
+
+    List<UserDTO> getAllUsers() throws SQLException;
 }
+
