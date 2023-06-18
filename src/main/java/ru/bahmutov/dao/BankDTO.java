6134aee5766095d2a4d9c2105114115606ec59f9
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 15:41:16 +0300

    add @ToString in dao

--- src/main/java/ru/bahmutov/dao/BankDTO.java
+++ src/main/java/ru/bahmutov/dao/BankDTO.java
@@ -4,10 +4,12 @@ package ru.bahmutov.dao;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class BankDTO {
     private Long id;
     private String name;
--- src/main/java/ru/bahmutov/dao/UserDTO.java
+++ src/main/java/ru/bahmutov/dao/UserDTO.java
@@ -3,10 +3,12 @@ package ru.bahmutov.dao;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class UserDTO {
     private Long id;
     private String fullName;
