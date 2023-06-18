70c284be571c5671451cca830c08ebaab7ef99f0
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-18 21:30:38 +0300

    add hibernate anotation to models

--- src/main/java/ru/bahmutov/models/User.java
+++ src/main/java/ru/bahmutov/models/User.java
@@ -1,13 +1,24 @@
 package ru.bahmutov.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "person")
+@Setter
+@Getter
 public class User {
+    @Id
+    @Column(name = "id")
     private Long id;
+    @Column(name = "full_name")
     private String fullName;
 }
