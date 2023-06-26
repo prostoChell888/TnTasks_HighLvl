59eccd9620cfd1b77c93f5fa45fccbe5cede2a3d
Александр Бахмутов <alexdmitralex@gmail.com>
2023-06-26 16:09:57 +0300

    change repository test

--- src/test/java/ru/bahmutov/repositories/NoteRepositoryTest.java
+++ src/test/java/ru/bahmutov/repositories/NoteRepositoryTest.java
@@ -1,13 +1,11 @@
 package ru.bahmutov.repositories;
 
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -29,12 +27,19 @@ class NoteRepositoryTest {
     private NoteRepository noteRepository;
 
     @Test
-    void shouldSaveNote() {
-         noteRepository.save(new Note(null, "qq", "qq"));
+    @DisplayName("should get note by id ")
+    void shouldGetNoteById() {
+         noteRepository.save(new Note(null, "qq1", "qq2"));
 
-        var res = noteRepository.findAll();
+        var resNote = noteRepository.findById(1L).get();
 
-        assertEquals(1, res.size());
+        assertEquals(1L, resNote.getId());
+        assertEquals("qq1", resNote.getBody());
+        assertEquals("qq2", resNote.getAuthor());
+
+
     }
+
+
 
 }
