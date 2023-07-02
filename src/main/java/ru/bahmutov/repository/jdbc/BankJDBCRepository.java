f4482dd0dc0646dadb75a0b53f556289d63abd51
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-02 07:15:32 +0300

    add get by id

--- src/main/java/ru/bahmutov/repository/jdbc/BankJDBCRepository.java
+++ src/main/java/ru/bahmutov/repository/jdbc/BankJDBCRepository.java
@@ -5,6 +5,7 @@ import ru.bahmutov.dao.BankDTO;
 import ru.bahmutov.repository.BankRepository;
 
 import java.sql.*;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,6 +25,7 @@ public class BankJDBCRepository implements BankRepository {
 
     @Override
     public List<BankDTO> updateBankNames(String newName) throws SQLException {
+        if (newName == null) return Collections.emptyList();
         String updateQuery = "UPDATE bank SET name = ?";
         try (Connection connection = DriverManager.getConnection(config.getUrl(), config.getUsername(),
                 config.getPassword())) {
@@ -48,14 +50,20 @@ public class BankJDBCRepository implements BankRepository {
     public List<BankDTO> getAllBunks() throws SQLException {
         String selectQuery = "SELECT * FROM bank";
         List<BankDTO> listOfBanks = new LinkedList<>();
+
         try (Connection connection = DriverManager.getConnection(config.getUrl(), config.getUsername(),
                 config.getPassword())) {
-
+            connection.setAutoCommit(false);
             try (var queryStatement = connection.createStatement();
                  var resSet = queryStatement.executeQuery(selectQuery)) {
                 while (resSet.next()) {
                     listOfBanks.add(new BankDTO(resSet.getLong("id"), resSet.getString("name")));
                 }
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+            } finally {
+                connection.setAutoCommit(true);
             }
         }
 
@@ -63,16 +71,49 @@ public class BankJDBCRepository implements BankRepository {
     }
 
     @Override
-    public void save(BankDTO bank) throws SQLException {
-        String selectQuery = "INSERT INTO bank (name) " +
-                "VALUES (?)";
-        List<BankDTO> listOfBanks = new LinkedList<>();
+    public BankDTO getById(long id) throws SQLException {
+        String query = "SELECT * FROM bank WHERE id = ?";
+        BankDTO result = null;
+        try (Connection connection = DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword())) {
+            connection.setAutoCommit(false);
+            try (var statement = connection.prepareStatement(query)) {
+                statement.setLong(1, id);
+                var resSet = statement.executeQuery();
+                resSet.next();
+                result = new BankDTO(resSet.getLong("id"), resSet.getString("name"));
+            } catch (Exception e) {
+                connection.rollback();
+            } finally {
+                connection.setAutoCommit(true);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public BankDTO save(BankDTO bank) throws SQLException {
+        if (bank == null) return null;
+
+        String query = "INSERT INTO bank (name) VALUES (?)";
+        Long generatedId = null;
         try (Connection connection = DriverManager.getConnection(config.getUrl(), config.getUsername(),
                 config.getPassword())) {
-            try (var statement =connection.prepareStatement(selectQuery);) {
+            connection.setAutoCommit(false);
+            try (var statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
                 statement.setString(1, bank.getName());
+                statement.executeUpdate();
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next())
+                        generatedId = generatedKeys.getLong(1);
+                }
+            } catch (Exception e) {
+                connection.rollback();
+            } finally {
+                connection.setAutoCommit(true);
             }
-
         }
+
+        return (generatedId == null) ? null : getById(generatedId);
     }
 }
