ef226ff6068f8ee3ddbacfaaf40e29d3d9c870f3
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-02 07:10:54 +0300

    add constructor only with param name

--- src/main/java/ru/bahmutov/dao/BankDTO.java
+++ src/main/java/ru/bahmutov/dao/BankDTO.java
@@ -1,16 +1,18 @@
 package ru.bahmutov.dao;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class BankDTO {
+    public BankDTO(String name) {
+        this.name = name;
+    }
+
     private Long id;
     private String name;
 }
