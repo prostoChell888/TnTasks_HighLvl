80879128ee5a6952a039cc32c182deab75f7ef3e
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-02 10:03:32 +0300

    clean main

--- src/main/java/ru/bahmutov/Main.java
+++ src/main/java/ru/bahmutov/Main.java
@@ -1,16 +1,7 @@
 package ru.bahmutov;
 
-import ru.bahmutov.configuration.ConfigurationDB;
-import ru.bahmutov.repository.UserRepository;
-import ru.bahmutov.repository.jdbc.UserJDBCRepository;
-
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        var prop = new ConfigurationDB("application.properties");
-        UserRepository repository = new UserJDBCRepository(prop);
-        var res = repository.getAllUsers();
-        res.forEach(System.out::println);
+    public static void main(String[] args) {
+
     }
 }
