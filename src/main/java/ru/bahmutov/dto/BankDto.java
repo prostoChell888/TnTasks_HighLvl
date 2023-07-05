edeee5f361132b6a6abafaf21f06c198c6b2db23
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-05 17:50:41 +0300

    add equals and hashCode

--- src/main/java/ru/bahmutov/dto/BankDto.java
+++ src/main/java/ru/bahmutov/dto/BankDto.java
@@ -3,6 +3,8 @@ package ru.bahmutov.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 
 @Getter
 @AllArgsConstructor
@@ -12,4 +14,21 @@ public class BankDto {
 
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BankDto bankDto = (BankDto) o;
+
+        if (!Objects.equals(id, bankDto.id)) return false;
+        return Objects.equals(name, bankDto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/ru/bahmutov/dto/PersonDto.java
+++ src/main/java/ru/bahmutov/dto/PersonDto.java
@@ -4,9 +4,29 @@ package ru.bahmutov.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor
 public class PersonDto {
     private Long id;
     private String fullName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonDto personDto = (PersonDto) o;
+
+        if (!Objects.equals(id, personDto.id)) return false;
+        return Objects.equals(fullName, personDto.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+        return result;
+    }
 }
