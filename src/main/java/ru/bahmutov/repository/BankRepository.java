05903560e4dc7bb2e905cc3881a507a802a99d2d
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-06 06:48:30 +0300

    dell @Modifying

--- src/main/java/ru/bahmutov/repository/BankRepository.java
+++ src/main/java/ru/bahmutov/repository/BankRepository.java
@@ -1,7 +1,6 @@
 package ru.bahmutov.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.bahmutov.models.Bank;
@@ -11,7 +10,6 @@ import java.util.List;
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
 
-    @Modifying
-    @Query("UPDATE Bank b SET b.name = :newName")
+    @Query("UPDATE Bank b SET b.name = :newName ")
     List<Bank> updateBankNames(String newName);
 }
