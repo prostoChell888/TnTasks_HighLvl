b72ee10c93a5704423f0af2636756eb016b91c9c
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-06 06:46:54 +0300

    add init shema.sql

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS person
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR,
+
+    CONSTRAINT PK_person_id PRIMARY KEY (id)
+
+    );
+
+
+CREATE TABLE IF NOT EXISTS bank
+(
+    id   BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+
+    CONSTRAINT PK_bank_id PRIMARY KEY (id)
+    );
+
+
+CREATE TABLE IF NOT EXISTS person_bank
+(
+    id        BIGINT GENERATED ALWAYS AS IDENTITY,
+    person_id INTEGER NOT NULL REFERENCES person,
+    bank_id   INTEGER NOT NULL REFERENCES bank,
+    UNIQUE (person_id, bank_id),
+
+    CONSTRAINT PK_person_bank_id PRIMARY KEY (id)
+    );
