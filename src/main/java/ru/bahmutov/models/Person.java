b7c1c98fe8e97824b8c92801c881b28e39860667
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-06 07:16:31 +0300

    add equals

--- src/main/java/ru/bahmutov/models/Person.java
+++ src/main/java/ru/bahmutov/models/Person.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -19,4 +21,22 @@ public class Person {
     private Long id;
     @Column(name = "full_name")
     private String fullName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!Objects.equals(id, person.id)) return false;
+        return Objects.equals(fullName, person.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+        return result;
+    }
 }
