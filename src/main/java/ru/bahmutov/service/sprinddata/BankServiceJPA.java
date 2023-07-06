672969231b2e72ef19215091459c32dfbaacc046
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-06 16:49:26 +0300

    add validation

--- src/main/java/ru/bahmutov/service/sprinddata/BankServiceJPA.java
+++ src/main/java/ru/bahmutov/service/sprinddata/BankServiceJPA.java
@@ -1,7 +1,9 @@
 package ru.bahmutov.service.sprinddata;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import ru.bahmutov.models.Bank;
 import ru.bahmutov.repository.BankRepository;
 import ru.bahmutov.service.BankService;
@@ -10,12 +12,14 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class BankServiceJPA implements BankService {
 
     private final BankRepository repository;
 
     @Override
-    public List<Bank> updateBankNames(String newName) {
-        return repository.updateBankNames(newName);
+    public List<Bank> updateBankNames(@NotBlank String newName) {
+        repository.updateBankNames(newName);
+        return repository.findAll();
     }
 }
