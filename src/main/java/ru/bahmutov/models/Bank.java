2112e5fc186bd95b3fc251fc8327e8e5ebd03d79
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-06 06:48:02 +0300

    add equals to Bank

--- src/main/java/ru/bahmutov/models/Bank.java
+++ src/main/java/ru/bahmutov/models/Bank.java
@@ -10,6 +10,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -22,4 +24,22 @@ public class Bank {
     private Long id;
     @Column(name = "name")
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bank bank = (Bank) o;
+
+        if (!Objects.equals(id, bank.id)) return false;
+        return Objects.equals(name, bank.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
