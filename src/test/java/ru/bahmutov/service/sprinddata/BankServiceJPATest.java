f727bcec5f4e39f77d7b007d03e3e9901fb58fe0
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-08 20:15:39 +0300

    change test

---
+++ src/test/java/ru/bahmutov/service/sprinddata/BankServiceJPATest.java
@@ -0,0 +1,14 @@
+package ru.bahmutov.service.sprinddata;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceJPATest {
+
+    @Test
+    @DisplayName("should Update all bank names")
+    void shouldUpdateBankNames() {
+    }
+}
