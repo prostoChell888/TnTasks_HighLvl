b678aecec1524db3ac8dd46fb6fa9c1954d94160
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-09 07:29:37 +0300

    add toString

--- src/main/java/ru/bahmutov/dto/BankDto.java
+++ src/main/java/ru/bahmutov/dto/BankDto.java
@@ -2,12 +2,14 @@ package ru.bahmutov.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.Objects;
 
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class BankDto {
 
     private Long id;
--- src/main/java/ru/bahmutov/dto/PersonDto.java
+++ src/main/java/ru/bahmutov/dto/PersonDto.java
@@ -3,11 +3,13 @@ package ru.bahmutov.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class PersonDto {
     private Long id;
     private String fullName;
