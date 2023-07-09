caa7eccd55315693d5ef28b794216e7ebcdefc07
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-09 18:36:02 +0300

    add Transactional

--- src/main/java/ru/bahmutov/repository/BankRepository.java
+++ src/main/java/ru/bahmutov/repository/BankRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bahmutov.models.Bank;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
 
     @Modifying
+    @Transactional
     @Query("UPDATE Bank b SET b.name = :newName ")
     void updateBankNames(String newName);
 }
