b4665c81876a53824ba5ef317b846210b40d5969
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-09 07:30:05 +0300

    add main

--- src/main/java/ru/bahmutov/Main.java
+++ src/main/java/ru/bahmutov/Main.java
@@ -1,6 +1,22 @@
 package ru.bahmutov;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.bahmutov.service.BankService;
+import ru.bahmutov.service.PersonService;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        var conf = SpringApplication.run(Main.class, args);
+        var bankService = conf.getBean(BankService.class);
+        var personService = conf.getBean(PersonService.class);
+
+        var banks = bankService.updateBankNames("new bank name");
+        banks.forEach(System.out::println);
+
+        var allPeople = personService.getAllPeople();
+        allPeople.forEach(System.out::println);
     }
 }
